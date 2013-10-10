14ac868085409c670b5e35bc0e6befa030428c15
Lucas Bickel <hairmare@purplehaze.ch>
2013-10-10 23:05:19 +0200

    add missing use

--- src/Graviton/MessagingBundle/GravitonMessagingBundle.php
+++ src/Graviton/MessagingBundle/GravitonMessagingBundle.php
@@ -4,6 +4,7 @@ namespace Graviton\MessagingBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use JDare\ClankBundle\JDareClankBundle;
+use Graviton\BundleBundle\GravitonBundleInterface;
 
 class GravitonMessagingBundle extends Bundle implements GravitonBundleInterface
 {
